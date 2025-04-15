c68edea2246ec9ca61120a106dbe9a71c59d44d4
rohitjg13 <jgrohit.exe@gmail.com>
2025-04-16 03:21:11 +0530

    Added Transparency

--- TaskApp/TaskAppApp.swift
+++ TaskApp/TaskAppApp.swift
@@ -4,7 +4,26 @@ import SwiftUI
 struct YourProjectApp: App {
     var body: some Scene {
         WindowGroup {
-            TaskDashboardView()
+            NavigationSplitView {
+                SidebarView(team: sampleTeam)
+                    .navigationSplitViewColumnWidth(min: 180, ideal: 190, max: 200)
+            }
+            detail: {
+                ContentView()
+            }
+            .background(.ultraThinMaterial)
+            .frame(maxWidth: .infinity, maxHeight: .infinity)
+            .onAppear {
+                makeWindowTransparent()
+            }
+        }
+    }
+    func makeWindowTransparent() {
+        for window in NSApplication.shared.windows {
+            window.isOpaque = false
+            window.backgroundColor = .clear
+            window.titleVisibility = .hidden
+            window.titlebarAppearsTransparent = true
         }
     }
 }
